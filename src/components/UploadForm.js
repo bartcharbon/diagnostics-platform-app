53fe5fb240f1a8a598e659a756057978984606c8
Mark-de-Haan <markdehaan90@gmail.com>
2016-09-28 17:01:51 +0200

    Add upload component to home screen

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, FormControl } from 'react-bootstrap'
 
 const propTypes = {
   width           : PropTypes.oneOf(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']),
@@ -9,41 +9,24 @@ const propTypes = {
 }
 
 class UploadForm extends Component {
-  render () {
-    var gridWidth = this.props.width ? 'col-md-' + this.props.width : 'col-md-12'
-    var actions = [
-      { value : 'ADD', label : 'ADD' },
-      { value : 'ADD_UPDATE_EXISTING', label : 'ADD/UPDATE' },
-      { value : 'UPDATE', label : 'UPDATE' }]
-    return <form>
-      <div className={gridWidth}>
-        <div className='form-group'>
-          <input type='file' onChange={this._setFile} />
-          {this.state.warning && <span id='helpBlock' className='help-block'>{this.state.warning}</span>}
-        </div>
+  constructor (props) {
+    super(props)
 
-        {this.state.showNameField &&
-        <div className='form-group'>
-          <label htmlFor='file-name-input-field'>Name</label>
-          <Input id='file-name-input-field' type='string' onValueChange={this._setFileName} required
-            value={this.state.fileName} />
-        </div>
-        }
-        {this.props.showAction && <div className='form-group'>
-          <label htmlFor='action-field'>Action</label>
-          <RadioGroup name='action-field' layout='vertical' options={actions} type='radio'
-            onValueChange={this._setAction} required={false} value={this.state.action} />
-        </div>}
+    // Bind this to functions
+    this.setFile = this.setFile.bind(this)
+    this.setFileName = this.setFileName.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
 
-        {this.state.file && <div className='form-group'>
-          <Button id='upload-file-btn' type='submit' style='default' size='medium' onClick={this._onSubmit}
-            text='Upload'
-            disabled={this.state.showNameField && !this.state.fileName} />
-        </div>}
-      </div>
-    </form>
+    // Initial state
+    this.state = {
+      file          : null,
+      showNameField : false,
+      fileName      : null,
+      action        : 'ADD'
+    }
   }
-  _setFile (event) {
+
+  setFile (event) {
     const file = event.target.files[0]
     var fileName = file.name.toLowerCase()
     var showNameField = fileName.endsWith('.vcf') || fileName.endsWith('.vcf.gz')
@@ -51,30 +34,51 @@ class UploadForm extends Component {
       this.setState({ warning : 'Invalid file name, extension must be ' + this.props.validExtensions })
     } else {
       if (showNameField) {
-      // Remove extension
+        // Remove extension
         fileName = fileName.replace(/\.vcf|\.vcf\.gz/, '')
-      // Maximum length is 30 chars, but we need to take into account that the samples are post fixed "_SAMPLES"
+        // Maximum length is 30 chars, but we need to take into account that the samples are post fixed "_SAMPLES"
         fileName = fileName.substring(0, 21)
-      // Remove illegal chars
+        // Remove illegal chars
         fileName = fileName.replace(/-|\.|\*|\$|&|%|\^|\(|\)|#|!|@|\?/g, '_')
-      // Don't allow entitynames starting with a number
+        // Don't allow entity names starting with a number
         fileName = fileName.replace(/^[0-9]/g, '_')
         this.setState({ fileName })
       }
       this.setState({ file, showNameField, warning : undefined })
     }
   }
-  _setFileName (fileName) {
+
+  setFileName (fileName) {
     this.setState({ fileName : fileName.value })
   }
 
-  _setAction (action) {
-    this.setState({ action : action.value })
-  }
-
-  _onSubmit (event) {
+  onSubmit (event) {
     event.preventDefault()
     this.props.onSubmit(this.state)
+  }
+
+  render () {
+    var gridWidth = this.props.width ? 'col-md-' + this.props.width : 'col-md-12'
+    return (<form>
+      <div className={gridWidth}>
+        <div className='form-group'>
+          <input type='file' onChange={this.setFile} />
+          {this.state.warning && <span id='helpBlock' className='help-block'>{this.state.warning}</span>}
+        </div>
+
+        {this.state.showNameField &&
+        <FormControl
+          type='text'
+          value={this.state.fileName}
+          placeholder='Entity name'
+          onChange={this.setFileName}
+        />}
+
+        {this.state.file && <div className='form-group'>
+          <Button bsStyle='default' onClick={this.onSubmit} disabled={this.state.showNameField && !this.state.fileName}>Upload</Button>
+        </div>}
+      </div>
+    </form>)
   }
 }
 
--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,16 +1,38 @@
 import React from 'react'
-import classes from './HomeView.scss'
 import UploadForm from 'components/UploadForm'
+// import { post } from 'redux/modules/MolgenisApi'
+import fetch from 'isomorphic-fetch'
+
+
+
+const validExtensions = ['vcf', 'xlsx', 'xls', 'vcf.gz']
+
+function onSubmit (form) {
+    var data = new FormData();
+    data.append('file', form.file);
+    data.append('entityName', form.fileName);
+    data.append('action', form.action);
+    data.append('notify', false);
+
+    console.log('post with data: ', data)
+
+    fetch('http://localhost:8080/plugin/importwizard/importFile', {
+        mode: 'no-cors',
+        method : 'post',
+        body   : data
+    })
+}
+
 
 export const HomeView = () => (
   <div>
     <h4>Welcome!</h4>
     <p>Upload your VCF file with gene annotations</p>
     <UploadForm
-      width={6}
-      showAction={false}
-      validExtensions={['vcf', 'xlsx', 'xls', 'vcf.gz']}
-      onSubmit={(x) => { console.log('onSubmit', x) }}
+        width={'12'}
+        showAction={false}
+        onSubmit={onSubmit}
+        validExtensions={validExtensions}
     />
   </div>
 )
